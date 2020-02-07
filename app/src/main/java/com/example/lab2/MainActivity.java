3f306903c9d4e5337dc2f67c53cbb06890aab00d
Sam Ramakrishnan <sramakrishn8@wisc.edu>
2020-02-06 23:16:32 -0600

    Milestone 1

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.lab2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText txtInput;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtInput = findViewById(R.id.txtInput);
+
+
+    }
+
+    public void btnListener(View view) {
+
+        Log.d("dbg",txtInput.getText().toString());
+
+        Toast.makeText(this,txtInput.getText().toString(), Toast.LENGTH_LONG).show();
     }
 }
